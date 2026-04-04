1ca515055157d69d844f3317b8274bc96a73d314
thaiherbcenters <thaiherbcenters@gmail.com>
2026-04-04 09:47:07 +0700

    Create socket.js

---
+++ Client/src/core/socket.js
@@ -0,0 +1,9 @@
+import { io } from "socket.io-client";
+
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+const defaultUrl = isLocalhost ? "http://localhost:3000" : `http://${window.location.hostname}:3000`;
+const URL = import.meta.env.VITE_SERVER_URL || defaultUrl;
+
+export const socket = io(URL, {
+  transports: ["websocket"],
+});
