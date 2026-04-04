29ea0049d965e9ad3dea3e3999d5803fb9a71eaa
thaiherbcenters <thaiherbcenters@gmail.com>
2026-04-04 09:47:31 +0700

    Create collaboration.js

---
+++ Server/socket/collaboration.js
@@ -0,0 +1,34 @@
+const store = require("../state/store");
+
+module.exports = (io, socket) => {
+
+  socket.on("cursor-move", (data) => {
+    socket.broadcast.emit("cursor-move", {
+      id: socket.id,
+      ...data,
+    });
+  });
+
+  socket.on("laser", (data) => {
+    const user = store.users[socket.id];
+    socket.broadcast.emit("laser", {
+      id: socket.id,
+      ...data,
+      name: user?.name || "?",
+      color: user?.color || "#ef4444",
+    });
+  });
+
+  socket.on("disconnect", () => {
+    store.connectedUsers--;
+    delete store.users[socket.id];
+
+    if (store.hostSocketId === socket.id) {
+      store.hostSocketId = null;
+      io.emit("host-exists", { exists: false });
+    }
+
+    socket.broadcast.emit("user-left", { id: socket.id });
+    io.emit("user-count", store.connectedUsers);
+  });
+};
